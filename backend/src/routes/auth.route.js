bd039549a0265636f3722da040f6f542575ce639
IqbalShahed <afranshahed@gmail.com>
2025-11-08 10:52:49 +0600

    API Security implemented by Arcjet

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import { signup, login, logout, updateProfile } from '../controllers/auth.controller.js';
-import { loginLimiter, protectRoute } from '../middlewares/auth.middleware.js';
+import { protectRoute } from '../middlewares/auth.middleware.js';
+import { arcjetProtection } from '../middlewares/arcjet.middleware.js';
 
 const router = express.Router();
 
+router.use(arcjetProtection);
+
 router.post("/signup", signup);
-router.post("/login", loginLimiter, login);
+router.post("/login", login);
 router.post("/logout", logout);
 router.put("/update-profile", protectRoute, updateProfile);
 router.get("/check", protectRoute, (req, res) => res.status(200).json(req.user))
